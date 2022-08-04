d4738bf01c3de9f6fced1259fe35e30e84a74c40
liuchang <liuchang_njyg@piesat.cn>
2022-08-04 14:37:43 +0800

    新增gateway子模块，增加gateway依赖，并注册到注册中心

--- gateway/src/main/java/com/course/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/course/gateway/GatewayApplication.java
@@ -1,10 +1,10 @@
-package com.course.system;
+package com.course.gateway;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.core.env.Environment;
 
 /**
@@ -12,15 +12,15 @@ import org.springframework.core.env.Environment;
  * date 2022-08-04
  */
 @SpringBootApplication
-@EnableEurekaServer
-public class SystemApplication {
+@EnableEurekaClient
+public class GatewayApplication {
 
-	private static final Logger LOG = LoggerFactory.getLogger(SystemApplication.class);
+	private static final Logger LOG = LoggerFactory.getLogger(GatewayApplication.class);
 
 	public static void main(String[] args) {
-		SpringApplication app = new SpringApplication(SystemApplication.class);
+		SpringApplication app = new SpringApplication(GatewayApplication.class);
 		Environment env = app.run(args).getEnvironment();
 		LOG.info("启动成功！！");
-		LOG.info("System地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
+		LOG.info("Gateway地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
 	}
 }
