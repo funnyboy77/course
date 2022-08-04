000e57c9e9bcce2c2ffc3c9fb37fab171603fd0f
liuchang <liuchang_njyg@piesat.cn>
2022-08-04 13:25:29 +0800

    将system注册到eureka

--- system/src/main/java/com/course/system/SystemApplication.java
+++ system/src/main/java/com/course/system/SystemApplication.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.core.env.Environment;
 
 /**
@@ -12,6 +13,7 @@ import org.springframework.core.env.Environment;
  * date 2022-08-04
  */
 @SpringBootApplication
+@EnableEurekaServer
 public class SystemApplication {
 
 	private static final Logger LOG = LoggerFactory.getLogger(EurekaApplication.class);
