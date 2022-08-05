b9ba9e96492d3df5337d510e14a9125448f6bf25
liuchang <liuchang_njyg@piesat.cn>
2022-08-05 18:10:48 +0800

    修复无法调用service的bug

--- server/src/main/java/com/course/server/mapper/TestMapper.java
+++ server/src/main/java/com/course/server/mapper/TestMapper.java
@@ -1,6 +1,7 @@
 package com.course.server.mapper;
 
 import com.course.server.domain.Test;
+import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
@@ -9,6 +10,7 @@ import java.util.List;
  * @Description 测试mapper
  * @date 2022/8/4
  */
+@Mapper
 public interface TestMapper {
 
      List<Test> list();
--- server/src/main/java/com/course/server/service/TestService.java
+++ server/src/main/java/com/course/server/service/TestService.java
@@ -1,18 +1,24 @@
 package com.course.server.service;
 
 import com.course.server.domain.Test;
+import com.course.server.mapper.TestMapper;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 /**
  * @author liuchang
- * @Description 测试接口
+ * @Description 测试实现类
  * @date 2022/8/4
  */
 @Service
-public interface TestService {
+public class TestService{
 
-     List<Test> list();
+    @Resource
+    TestMapper testMapper;
 
+    public List<Test> list() {
+        return testMapper.list();
+    }
 }
