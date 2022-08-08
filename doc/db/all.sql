b5f2ea44612e4c258c1cc3e2e7ea70f1b145cb96
liuchang <liuchang_njyg@piesat.cn>
2022-08-08 15:55:26 +0800

    增加大章chapter表sql，生成持久层代码

--- doc/db/all.sql
+++ doc/db/all.sql
@@ -1,3 +1,4 @@
+# -------测试
 drop table if exists `test`;
 create table `test`(
     `id` char(8) not null default '' comment 'id',
@@ -6,3 +7,13 @@ create table `test`(
 )engine=innodb default character set utf8mb4 comment ='测试';
 
 insert into `test` (id,name) values (2,'测试');
+
+# ----------大章
+drop table if exists `chapter`;
+create table `chapter`(
+     `id` char(8) not null default '' comment 'id',
+     `course_id` char(8) comment '课程ID',
+     `name` varchar(50) comment '名称',
+     primary key (`id`)
+)engine=innodb default character set utf8mb4 comment ='大章';
+
