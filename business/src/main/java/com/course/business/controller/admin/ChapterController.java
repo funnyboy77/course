4442c4dc2efc515ccd458de2eeb8a1ad5906e762
liuchang <liuchang_njyg@piesat.cn>
2022-08-08 16:23:53 +0800

    增加dto层，用于controller和service层 增加ChapterDto 修改ChapterService，将返回Chapter改成返回ChapterDto

--- business/src/main/java/com/course/business/controller/admin/ChapterController.java
+++ business/src/main/java/com/course/business/controller/admin/ChapterController.java
@@ -1,7 +1,8 @@
-package com.course.business.controller;
+package com.course.business.controller.admin;
 
 
 import com.course.server.domain.Chapter;
+import com.course.server.dto.ChapterDto;
 import com.course.server.service.ChapterService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,13 +15,14 @@ import java.util.List;
  * date 2022-08-04
  */
 @RestController
+@RequestMapping("/admin")
 public class ChapterController {
 
     @Resource
     ChapterService chapterService;
 
     @RequestMapping("/chapter")
-    public List<Chapter> chapter() {
+    public List<ChapterDto> chapter() {
         return chapterService.list();
     }
 
