64499fe72873dc0660b08f7d53768c33bc1ba79a
liuchang <liuchang_njyg@piesat.cn>
2022-08-09 11:03:25 +0800

    解决gateway跨域问题

--- server/src/main/java/com/course/server/config/CorsConfig.java
+++ server/src/main/java/com/course/server/config/CorsConfig.java
@@ -1,26 +1,26 @@
-package com.course.server.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-/**
- * @author liuchang
- * date 2022-08-09
- */
-@Configuration
-public class CorsConfig implements WebMvcConfigurer {
-
-   @Override
-   public void addCorsMappings(CorsRegistry registry) {
-       registry.addMapping("/**")
-               .allowedOrigins("*")
-               .allowedHeaders(CorsConfiguration.ALL)
-               .allowedMethods(CorsConfiguration.ALL)
-               .allowCredentials(true)
-               // 1小时内不需要再预检（发OPTIONS请求）
-               .maxAge(3600);
-   }
-
-}
+// package com.course.server.config;
+//
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.cors.CorsConfiguration;
+// import org.springframework.web.servlet.config.annotation.CorsRegistry;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+// /**
+//  * @author liuchang
+//  * date 2022-08-09
+//  */
+// @Configuration
+// public class CorsConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("*")
+//                .allowedHeaders(CorsConfiguration.ALL)
+//                .allowedMethods(CorsConfiguration.ALL)
+//                .allowCredentials(true)
+//                // 1小时内不需要再预检（发OPTIONS请求）
+//                .maxAge(3600);
+//    }
+//
+// }
