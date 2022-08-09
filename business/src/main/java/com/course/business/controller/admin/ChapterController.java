d5234904b6dcf54491b2f46b63736f1ec24667d4
liuchang <liuchang_njyg@piesat.cn>
2022-08-09 09:41:26 +0800

    集成axios完成前后端交互 增加CorsConfig，解决前后端跨域的问题

--- business/src/main/java/com/course/business/controller/admin/ChapterController.java
+++ business/src/main/java/com/course/business/controller/admin/ChapterController.java
@@ -15,14 +15,14 @@ import java.util.List;
  * date 2022-08-04
  */
 @RestController
-@RequestMapping("/admin")
+@RequestMapping("/admin/chapter")
 public class ChapterController {
 
     @Resource
     ChapterService chapterService;
 
-    @RequestMapping("/chapter")
-    public List<ChapterDto> chapter() {
+    @RequestMapping("/list")
+    public List<ChapterDto> list() {
         return chapterService.list();
     }
 
