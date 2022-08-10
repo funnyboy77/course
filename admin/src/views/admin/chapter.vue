7dc5cb9a814e4b289fc4caae67a4995a372b7342
liuchang <liuchang_njyg@piesat.cn>
2022-08-10 15:15:28 +0800

    修改index文件

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -136,6 +136,9 @@ export default {
         if (resp.success) {
           $("#form-modal").modal("hide");
           _this.list(1);
+          Toast.success("保存成功");
+        }else {
+          Toast.warning(resp.message);
         }
       })
     },
