cc38e86d8eba119c6681590167fa56ee05dc1e56
liuchang <liuchang_njyg@piesat.cn>
2022-08-10 14:40:18 +0800

    增加工具栏tools.js和校验类validator.js 大章保存非空和长度校验

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -121,6 +121,14 @@ export default {
 
     save() {
       let _this = this;
+      //保存校验
+      if (!Validator.require(_this.chapter.name, "名称")
+          ||!Validator.require(_this.chapter.courseId, "课程ID")
+          ||!Validator.length(_this.chapter.courseId, "课程ID", 1, 8)) {
+        return;
+      }
+
+      Loading.show();
       _this.$ajax.post('http://127.0.0.1:9000/business/admin/chapter/save',this.chapter
       ).then((response) => {
         console.log("保存新增大章结果：", response);
