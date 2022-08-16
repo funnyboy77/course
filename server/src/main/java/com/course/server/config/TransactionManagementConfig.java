2567e878867ca07f80863895a8901056b1708431
liuchang <liuchang_njyg@piesat.cn>
2022-08-16 09:25:53 +0800

    增加事务配置

---
+++ server/src/main/java/com/course/server/config/TransactionManagementConfig.java
@@ -0,0 +1,15 @@
+package com.course.server.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * @author liuchang
+ * @Description 事务处理
+ * @date 2022/8/16
+ */
+@EnableTransactionManagement
+@Configuration
+public class TransactionManagementConfig {
+
+}
