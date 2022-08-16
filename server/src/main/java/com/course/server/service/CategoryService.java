827882fa2f318b23dc7d016b818757caebc7e09a
liuchang <liuchang_njyg@piesat.cn>
2022-08-16 11:02:04 +0800

    两级分类的新增修改删除

--- server/src/main/java/com/course/server/service/CategoryService.java
+++ server/src/main/java/com/course/server/service/CategoryService.java
@@ -10,6 +10,7 @@ import com.course.server.util.UuidUtil;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
 import javax.annotation.Resource;
@@ -82,8 +83,23 @@ public class CategoryService {
     /**
      * 删除
      */
+    @Transactional
     public void delete(String id) {
+        deleteChildren(id);
         categoryMapper.deleteByPrimaryKey(id);
+    }
+
+    /**
+     * 删除子分类
+     */
+    public void deleteChildren(String id) {
+        Category category = categoryMapper.selectByPrimaryKey(id);
+        if ("00000000".equals(category.getParent())) {
+            //如果是一级分类，需要删除其下的二级分类
+            CategoryExample categoryExample = new CategoryExample();
+            categoryExample.createCriteria().andParentEqualTo(category.getId());
+            categoryMapper.deleteByExample(categoryExample);
+        }
     }
 
 }
