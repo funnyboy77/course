b9fdc52e578aef7dc8981b384d86851b2fb6fdb4
liuchang <liuchang_njyg@piesat.cn>
2022-08-17 10:51:18 +0800

    文件上传功能开发：文件按用途分类保存

--- admin/src/views/admin/teacher.vue
+++ admin/src/views/admin/teacher.vue
@@ -88,6 +88,7 @@
                   <file v-bind:input-id="'image-upload'"
                         v-bind:text="'上传头像'"
                         v-bind:suffixs="['jpg','jpeg','png']"
+                        v-bind:use="FILE_USE.TEACHER.key"
                         v-bind:after-upload="afterUpload"></file>
                   <div v-show="teacher.image" class="row">
                     <div class="col-md-4">
@@ -137,6 +138,7 @@ export default {
     return {
       teacher: {},
       teachers: [],
+      FILE_USE: FILE_USE,
     }
   },
   mounted: function () {
