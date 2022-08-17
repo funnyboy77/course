d46517867b55485194f7d9099d21f379e8356931
liuchang <liuchang_njyg@piesat.cn>
2022-08-17 10:55:52 +0800

    文件上传功能开发：文件上传成功后返回值的处理

--- file/src/main/java/com/course/file/controller/admin/UploadController.java
+++ file/src/main/java/com/course/file/controller/admin/UploadController.java
@@ -74,7 +74,8 @@ public class UploadController {
         fileDto.setUse(use);
         fileService.save(fileDto);
         ResponseDto responseDto = new ResponseDto();
-        responseDto.setContent(FILE_DOMAIN + path);
+        fileDto.setPath(FILE_DOMAIN + path);
+        responseDto.setContent(fileDto);
         return responseDto;
     }
 
