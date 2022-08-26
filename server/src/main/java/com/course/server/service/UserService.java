4358405fbf85e59b1e6f92ed1a3af5d2266a894a
liuchang <liuchang_njyg@piesat.cn>
2022-08-26 15:05:09 +0800

    用户管理与登录: 增加用户名是否已存在校验 增加自定义业务异常

--- server/src/main/java/com/course/server/service/UserService.java
+++ server/src/main/java/com/course/server/service/UserService.java
@@ -4,12 +4,15 @@ import com.course.server.domain.User;
 import com.course.server.domain.UserExample;
 import com.course.server.dto.UserDto;
 import com.course.server.dto.PageDto;
+import com.course.server.exception.BusinessException;
+import com.course.server.exception.BusinessExceptionCode;
 import com.course.server.mapper.UserMapper;
 import com.course.server.util.CopyUtil;
 import com.course.server.util.UuidUtil;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 
 import javax.annotation.Resource;
@@ -55,6 +58,10 @@ public class UserService {
      */
     private void insert(User user) {
         user.setId(UuidUtil.getShortUuid());
+        User userDb = this.selectByLoginName(user.getLoginName());
+        if(userDb != null) {
+            throw new BusinessException(BusinessExceptionCode.USER_LOGIN_NAME_EXIST);
+        }
         userMapper.insert(user);
     }
 
@@ -72,4 +79,19 @@ public class UserService {
         userMapper.deleteByPrimaryKey(id);
     }
 
+    /**
+     * 根据登录名查询用户信息
+     * @param loginName
+     * @return
+     */
+    public User selectByLoginName(String loginName) {
+        UserExample userExample = new UserExample();
+        userExample.createCriteria().andLoginNameEqualTo(loginName);
+        List<User> userList = userMapper.selectByExample(userExample);
+        if (CollectionUtils.isEmpty(userList)) {
+            return null;
+        }else {
+            return userList.get(0);
+        }
+    }
 }
