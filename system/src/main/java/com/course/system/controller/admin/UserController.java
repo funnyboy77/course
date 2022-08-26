ad0e882e0e358725337046a993b91ac18c92e504
liuchang <liuchang_njyg@piesat.cn>
2022-08-26 15:36:25 +0800

    用户管理与登录: 增加重置密码功能，编辑用户信息的时候不修改密码

--- system/src/main/java/com/course/system/controller/admin/UserController.java
+++ system/src/main/java/com/course/system/controller/admin/UserController.java
@@ -58,6 +58,18 @@ public class UserController {
     }
 
     /**
+     * 重置密码
+     */
+    @PostMapping("/save-password")
+    public ResponseDto savePassword(@RequestBody UserDto userDto) {
+        userDto.setPassword(DigestUtils.md5DigestAsHex(userDto.getPassword().getBytes()));
+        ResponseDto responseDto = new ResponseDto();
+        userService.savePassword(userDto);
+        responseDto.setContent(userDto);
+        return responseDto;
+    }
+
+    /**
      * 删除
      */
     @RequestMapping("/delete/{id}")
