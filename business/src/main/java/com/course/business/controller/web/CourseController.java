1d631d0b20ac64a4250bff34a6bf8d9353fd2434
liuchang <liuchang_njyg@piesat.cn>
2022-08-30 17:30:27 +0800

    网站首页开发: 增加【新上好课】后端接口

---
+++ business/src/main/java/com/course/business/controller/web/CourseController.java
@@ -0,0 +1,43 @@
+package com.course.business.controller.web;
+
+
+import com.course.server.dto.*;
+import com.course.server.service.CourseCategoryService;
+import com.course.server.service.CourseService;
+import com.course.server.util.ValidatorUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * @author liuchang
+ * date 2022-08-04
+ */
+@RestController("webCourseController")
+@RequestMapping("/web/course")
+public class CourseController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(CourseController.class);
+    public static final String BUSINESS_NAME = "课程";
+
+    @Resource
+    CourseService courseService;
+
+    /**
+     * 列表查询,查询最新的3门已发布的课程
+     */
+    @GetMapping("/list-new")
+    public ResponseDto listNew() {
+        PageDto pageDto = new PageDto();
+        pageDto.setPage(1);
+        pageDto.setSize(3);
+        ResponseDto responseDto = new ResponseDto();
+        List<CourseDto> courseDtoList = courseService.listNew(pageDto);
+        responseDto.setContent(courseDtoList);
+        return responseDto;
+    }
+
+}
