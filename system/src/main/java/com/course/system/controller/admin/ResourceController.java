9f74974e0c381325a4dbab1caa794607e4175615
liuchang <liuchang_njyg@piesat.cn>
2022-08-30 11:38:38 +0800

    通用权限管理：资源树的生成

--- system/src/main/java/com/course/system/controller/admin/ResourceController.java
+++ system/src/main/java/com/course/system/controller/admin/ResourceController.java
@@ -2,6 +2,7 @@ package com.course.system.controller.admin;
 
 
 import com.course.server.dto.PageDto;
+import com.course.server.dto.ResourceDto;
 import com.course.server.dto.ResponseDto;
 import com.course.server.service.ResourceService;
 import com.course.server.util.ValidatorUtil;
@@ -10,6 +11,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @author liuchang
@@ -56,6 +58,17 @@ public class ResourceController {
     public ResponseDto delete(@PathVariable String id) {
         ResponseDto responseDto = new ResponseDto();
         resourceService.delete(id);
+        return responseDto;
+    }
+
+    /**
+     * 资源树查询
+     */
+    @GetMapping("/load-tree")
+    public ResponseDto loadTree() {
+        ResponseDto responseDto = new ResponseDto();
+        List<ResourceDto> resourceDtoList = resourceService.loadTree();
+        responseDto.setContent(resourceDtoList);
         return responseDto;
     }
 }
