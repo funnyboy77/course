e47a68a5119f1b13d955337cf0360b1ad2e6c3e8
liuchang <liuchang_njyg@piesat.cn>
2022-08-30 17:16:45 +0800

    网站首页开发: 大屏文案修改

--- web/src/components/the-footer.vue
+++ web/src/components/the-footer.vue
@@ -5,7 +5,7 @@
         <a href="#">回到顶部</a>
       </p>
       <p>欢迎使用慕课在线视频课程</p>
-      <p>2099&nbsp;imooc.com&nbsp;京ICP备 12345678<a href="/">Visit the homepage</a> or read our <a href="/docs/getting-started/introduction/">getting started guide</a>.</p>
+      <p>2022-2099&nbsp;imooc.com&nbsp;京ICP备 12345678<a href="/"></a></p>
     </div>
   </footer>
 </template>
