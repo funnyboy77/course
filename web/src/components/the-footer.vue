3631b547f683b285214872b63ba190bdd79e2f8c
liuchang <liuchang_njyg@piesat.cn>
2022-08-30 17:12:51 +0800

    网站首页开发: 底部文案修改

--- web/src/components/the-footer.vue
+++ web/src/components/the-footer.vue
@@ -2,10 +2,10 @@
   <footer class="text-muted">
     <div class="container">
       <p class="float-right">
-        <a href="#">Back to top</a>
+        <a href="#">回到顶部</a>
       </p>
-      <p>Album example is © Bootstrap, but please download and customize it for yourself!</p>
-      <p>New to Bootstrap? <a href="/">Visit the homepage</a> or read our <a href="/docs/getting-started/introduction/">getting started guide</a>.</p>
+      <p>欢迎使用慕课在线视频课程</p>
+      <p>2099&nbsp;imooc.com&nbsp;京ICP备 12345678<a href="/">Visit the homepage</a> or read our <a href="/docs/getting-started/introduction/">getting started guide</a>.</p>
     </div>
   </footer>
 </template>
